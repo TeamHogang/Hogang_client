c953fbb932128e2faaccdab130f5566fbb5b7a4d
wonho <wonho1401@naver.com>
2022-12-03 19:46:14 +0900

    [Feat] user, article API 작성

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
-  //   crossDomain: true,
-  // withCredentials: true,
+  crossDomain: true,
+  withCredentials: true,
 });
 
 export default api;
